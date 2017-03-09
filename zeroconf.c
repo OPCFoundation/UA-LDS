98e84720147c34c164656108b57e2b94bae3a23b
Stefan Profanter <git@s.profanter.me>
2017-03-09 11:58:48 +0100

    target field of SRV announce must end with .local

--- zeroconf.c
+++ zeroconf.c
@@ -385,9 +385,12 @@ OpcUa_StatusCode OPCUA_DLLCALL ualds_zeroconf_registerInternal(OpcUa_Void*  pvCa
         /* replace [gethostname] in own server name */
         if (bOwnRegistration != OpcUa_False)
         {
-            char hostname[UALDS_CONF_MAX_URI_LENGTH] = {0};
+            // additional 6 chars for '.local'
+            char hostname[UALDS_CONF_MAX_URI_LENGTH+6] = {0};
             ualds_platform_getfqhostname(hostname, sizeof(hostname));
             replace_string(szMDNSServerName, UALDS_CONF_MAX_URI_LENGTH, "[gethostname]", hostname);
+            // hostname must end with .local
+            sprintf(hostname + strlen(hostname), ".local");
             replace_string(szHostName, UALDS_CONF_MAX_URI_LENGTH, "[gethostname]", hostname);
             bOwnRegistration = OpcUa_False;
         }
