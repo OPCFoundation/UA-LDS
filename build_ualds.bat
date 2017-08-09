0694fae5506ea6bd467ef1f4dd1bfa2b2d039063
erhardgrishaber <gri@softing.com>
2017-08-09 11:06:32 +0300

    Merge from Jenkins Branch.

---
+++ build_ualds.bat
@@ -0,0 +1,46 @@
+@ECHO off
+REM ****************************************************************************************************************
+REM ** This script builds the UA-LDS.
+REM ** This must be run from a Visual Studio command line.
+REM ****************************************************************************************************************
+SETLOCAL
+
+set SRCDIR=%~dp0
+set INSTALLDIR=%~dp0
+set GIT=C:\Program Files\Git\bin\git.exe
+set SIGNTOOL=C:\Build\sign_output.bat
+
+IF "%1"=="no-clean" GOTO noClean
+ECHO STEP 1) Deleting Output Directories
+REM IF EXIST %INSTALLDIR%\bin rmdir /s /q %INSTALLDIR%\bin
+
+IF "%1"=="no-stack" GOTO noStack
+ECHO STEP 2) Fetch OpenSSL
+REM IF EXIST %INSTALLDIR%\stack\openssl rmdir /s /q %INSTALLDIR%\stack\openssl
+CALL fetch_openssl.bat
+
+ECHO STEP 3) Building Stack
+cd %SRCDIR%\stack
+CALL build_win32.bat
+:noStack
+
+ECHO STEP 4) Update Build Number
+IF NOT DEFINED BUILD_VERSION SET BUILD_VERSION=341
+IF NOT DEFINED BUILD_NUMBER  SET BUILD_NUMBER=1
+ECHO Building Version: %BUILD_VERSION%.%BUILD_NUMBER
+IF %BUILD_NUMBER% GTR 0 ECHO #define BUILD_NUMBER %BUILD_NUMBER% > buildversion.h
+
+ECHO STEP 4) Building LDS
+cd %SRCDIR%
+REM msbuild ualds.sln /p:Configuration=Release
+build_win32.bat
+
+ECHO STEP 5) Sign the Binaries
+#IF EXIST "%SIGNTOOL%" CALL "%SIGNTOOL%" %INSTALLDIR%\bin\Release\*.exe /dual
+IF EXIST "%SIGNTOOL%" CALL "%SIGNTOOL%" %INSTALLDIR%\build\bin\Release\*.exe /dual
+
+ECHO *** ALL DONE ***
+GOTO theEnd
+
+:theEnd
+ENDLOCAL
---
+++ fetch_openssl.bat
@@ -0,0 +1,19 @@
+@ECHO off
+REM ****************************************************************************************************************
+REM ** This script builds the debug version of OpenSSL for use by the ANSI C samples.
+REM ** It requires that PERL be installed in the path and it must be run from a Visual Studio command line.
+REM ****************************************************************************************************************
+SETLOCAL
+
+set ROOT=%~dp0
+set SRCDIR=openssl-1.0.2l
+set GITTAG=OpenSSL_1_0_2l
+set GIT=C:\Program Files\Git\bin\git.exe
+
+cd %ROOT%\stack
+IF NOT EXIST %SRCDIR% "%GIT%" clone https://github.com/openssl/openssl.git %SRCDIR%
+cd %SRCDIR%
+"%GIT%" checkout tags/%GITTAG%
+
+cd %ROOT%\stack
+perl -pi.back -e "s/openssl-1.0.\?\?/%SRCDIR%/" build_win32.bat
