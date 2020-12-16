4f5324b8f5a7923bd5d5de7741716e34a3ccc020
OPC-F SysAdmin <sysadmin@opcfoundation.org>
2020-12-16 01:38:05 -0800

    Update to OpenSSL_1_1_1i.

--- fetch_openssl.bat
+++ fetch_openssl.bat
@@ -6,8 +6,8 @@ REM ****************************************************************************
 SETLOCAL
 
 set ROOT=%~dp0
-set SRCDIR=openssl-1.1.1h
-set GITTAG=OpenSSL_1_1_1h
+set SRCDIR=openssl-1.1.1i
+set GITTAG=OpenSSL_1_1_1i
 
 set GIT=C:\Program Files\Git\bin\git.exe
 IF NOT EXIST "%GIT%" SET GIT=C:\Program Files (x86)\Git\bin\git.exe
