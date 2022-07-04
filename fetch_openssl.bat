c8c60f4b4772ee7134bd262e4f921c9ebc678886
github@opcfoundation.org <github@opcfoundation.org>
2022-07-04 10:57:31 -0700

    Upgrade to OpenSSL 1.1.1p.

--- fetch_openssl.bat
+++ fetch_openssl.bat
@@ -6,8 +6,8 @@ REM ****************************************************************************
 SETLOCAL
 
 set ROOT=%~dp0
-set SRCDIR=openssl-1.1.1n
-set GITTAG=OpenSSL_1_1_1n
+set SRCDIR=openssl-1.1.1p
+set GITTAG=OpenSSL_1_1_1p
 
 set GIT=C:\Program Files\Git\bin\git.exe
 IF NOT EXIST "%GIT%" SET GIT=C:\Program Files (x86)\Git\bin\git.exe
